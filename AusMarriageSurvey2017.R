5a7e2a76513a0a8344d6860fe5ccfa3e52d2ecb3
robotNoir <Angus@Anguss-iMac.local>
2017-12-10 21:01:22 +1100

    added mutation to survey data to get proportions added mutation and electorate filter to religious affiliation data to get proportions and remove unneeded GED codes added start of ggplot plots for analysis of religious affiliation and yes response

--- AusMarriageSurvey2017.R
+++ AusMarriageSurvey2017.R
@@ -46,6 +46,9 @@ CEDtoElectorate <- read_excel("~/AusMarriageSurvey2017/data/2016Census_geog_desc
   survey.df$Response_None <- as.integer(survey.df$Response_None)
   survey.df$Response_Total <- as.integer(survey.df$Response_Total)
   
+  survey.df <- survey.df %>%
+    mutate_at(vars(Response_Yes:Response_None), funs(. / Response_Total))
+  
   write.csv(survey.df, './data_clean/surveyresponsescleaned.csv')
   
   # CED-to-Electorate 
@@ -58,6 +61,51 @@ CEDtoElectorate <- read_excel("~/AusMarriageSurvey2017/data/2016Census_geog_desc
   relgiousaffiliation.df <- relgiousaffiliation.df %>%
     select(CED_CODE_2016, ends_with("_P")) %>%
     select(1:3,7,22:25,27,29:30,34,35) %>%
+    mutate(
+      Buddhism_P = Buddhism_P / Tot_P,
+      Christianity_Anglican_P = Christianity_Anglican_P / Tot_P,
+      Christianity_Catholic_P = Christianity_Catholic_P / Tot_P,
+      Hinduism_P = Hinduism_P / Tot_P,
+      Islam_P = Islam_P / Tot_P,
+      Judaism_P = Judaism_P / Tot_P,
+      Othr_Rel_Sikhism_P = Othr_Rel_Sikhism_P / Tot_P,
+      SB_OSB_NRA_NR_P = SB_OSB_NRA_NR_P / Tot_P,
+      Religious_affiliation_ns_P = Religious_affiliation_ns_P / Tot_P,
+      Other_Christianity = (Christianity_Tot_P - Christianity_Anglican_P - Christianity_Catholic_P) / Tot_P,
+      Other_Religions = (Other_Religions_Tot_P - Othr_Rel_Sikhism_P) / Tot_P
+    ) %>%
     left_join(CEDtoElectorate, by ="CED_CODE_2016") %>%
-    select(-CED_CODE_2016)
+    select(-CED_CODE_2016, -Christianity_Tot_P, -Other_Religions_Tot_P, -Tot_P) %>%
+    rename(
+      Buddhism = Buddhism_P,
+      Anglican = Christianity_Anglican_P,
+      Catholic = Christianity_Catholic_P,
+      Hinduism = Hinduism_P, 
+      Islam = Islam_P,
+      Judaism = Judaism_P,
+      Sikhism = Othr_Rel_Sikhism_P,
+      Secular_and_Other_Beliefs = SB_OSB_NRA_NR_P,
+      Affiliation_Not_Stated = Religious_affiliation_ns_P) %>%
+    gather(Religious_Affiliation, Religious_Fraction, Buddhism:Other_Religions) %>%
+    filter(!is.na(Religious_Fraction)) %>%
+    semi_join(survey.df, by="Electorate")
+    
+  relgiousaffiliation.df$Religious_Affiliation <- as.factor(relgiousaffiliation.df$Religious_Affiliation)
   
+  write.csv(relgiousaffiliation.df, './data_clean/religiousaffiliationscleaned.csv')
+  
+  # Catholic vs. Yes vote 
+  relgiousaffiliation.df %>%
+      ggplot(aes(x=Religious_Affiliation, y=Religious_Fraction)) +
+      geom_boxplot(alpha=0.6) +
+      theme_bw()
+  
+  relgiousaffiliation.df %>%
+    left_join(survey.df, by="Electorate") %>%
+    group_by(Religious_Affiliation) %>%
+    #filter(Religious_Affiliation == "Secular_and_Other_Beliefs") %>%
+    ggplot(aes(x=Religious_Fraction, y=Response_Yes, color=Religious_Affiliation)) +
+    geom_point(alpha=0.6) +
+    geom_smooth(alpha=0.6) +
+    facet_grid(Religious_Affiliation ~ .) +
+    theme_minimal()
